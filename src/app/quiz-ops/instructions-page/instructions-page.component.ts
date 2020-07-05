bfdb6115d840545a094beb851a775225b7c6583c
harshsit <harsh.raj@sitpune.edu.in>
2020-07-05 17:31:39 +0530

    UI commit

--- src/app/quiz-ops/instructions-page/instructions-page.component.ts
+++ src/app/quiz-ops/instructions-page/instructions-page.component.ts
@@ -9,14 +9,26 @@ import { Router } from '@angular/router';
 export class InstructionsPageComponent implements OnInit {
 
   continue: boolean = false;
-
+  hide1: boolean = true;
+hide:boolean=true;
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
   }
-
+  preview(){
+    if(this.hide1)
+      this.hide1 = false;
+    else
+      this.hide1= true;
+  }
+  preview1(){
+    if(this.hide)
+      this.hide = false;
+    else
+      this.hide= true;
+  }
   onClick(){
     if(this.continue === false)
       this.continue = true
