f194e07a3de53454d62821b09c0ef5bdfca861dc
ankitaroy <ankitaroy@gofynd.com>
2020-07-09 15:56:07 +0530

    added content

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+var json = require("./data.json");
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.less']
+  selector: "app-about-us",
+  templateUrl: "./about-us.component.html",
+  styleUrls: ["./about-us.component.less"],
 })
 export class AboutUsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    {
+      
+    }
   }
 
+  ngOnInit() {}
 }
