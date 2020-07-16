a277e2c4d9861e3bc1120f996083f7655c8ba777
ankitaroy <ankitaroy@gofynd.com>
2020-07-16 16:04:10 +0530

    WIP search in applied

--- src/app/candidate-ops/jobs-applied/jobs-applied.component.ts
+++ src/app/candidate-ops/jobs-applied/jobs-applied.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { QuizService } from 'src/app/shared/quiz.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { QuizService } from "src/app/shared/quiz.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-jobs-applied',
-  templateUrl: './jobs-applied.component.html',
-  styleUrls: ['./jobs-applied.component.less']
+  selector: "app-jobs-applied",
+  templateUrl: "./jobs-applied.component.html",
+  styleUrls: ["./jobs-applied.component.less"],
 })
 export class JobsAppliedComponent implements OnInit {
+  data: any = {};
+  query: string;
+  showLoader: boolean = false;
+  noMatch: boolean = false;
 
-  data: any =  {};
-
-  constructor(private quizService: QuizService,
-    private router: Router
-    ) { }
+  constructor(private quizService: QuizService, private router: Router) {}
 
   ngOnInit() {
-    this.getListing()
+    this.getListing();
   }
 
-  getListing(){
-    this.quizService.getAppliedJobs().subscribe(data => {
+  getListing() {
+    this.showLoader = true;
+    this.quizService.getAppliedJobs().subscribe((data) => {
       console.log(data);
       this.data = data;
+      this.showLoader = false;
+      if (this.data.count === 0) this.noMatch = true;
+      console.log(this.data);
     });
   }
 
+  clear() {
+    this.query = "";
+  }
 }
