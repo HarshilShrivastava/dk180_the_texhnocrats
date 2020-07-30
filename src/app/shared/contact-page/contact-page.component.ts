ac6a6059a46c3daa433f91bba180dd23ea17cd01
harshsit <harsh.raj@sitpune.edu.in>
2020-07-30 20:25:08 +0530

    Card Landing Page Revamop, Domain selection Image and Radio button issue resolved

--- src/app/shared/contact-page/contact-page.component.ts
+++ src/app/shared/contact-page/contact-page.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Fields } from "../signup-fields.model";
+import { Component, OnInit , Input} from '@angular/core';
+import { Contactfields } from "../contact-fields.model";
+import { from } from 'rxjs';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.less']
 })
 export class ContactPageComponent implements OnInit {
-  fields: Fields; 
+  @Input() public mode: any;
+
+  messages: Contactfields;
+  userNamePattern = "^[a-z0-9_]*$";
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
   constructor() { }
 
   ngOnInit() {
