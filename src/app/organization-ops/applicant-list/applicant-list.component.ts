cb42a8a14fd457a2121741e43fa06d05c582559b
gauravjoshi1998 <gauravjoshi1998@gmail.com>
2020-07-30 19:31:44 +0530

    Signup forms fixes

--- src/app/organization-ops/applicant-list/applicant-list.component.ts
+++ src/app/organization-ops/applicant-list/applicant-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { QuizService } from "src/app/shared/quiz.service";
 import { Router } from "@angular/router";
+import { MatDialog } from '@angular/material';
+import { JobViewDialogComponent } from 'src/app/shared/job-view-dialog/job-view-dialog.component';
 
 @Component({
   selector: "app-applicant-list",
@@ -14,7 +16,11 @@ export class ApplicantListComponent implements OnInit {
   showLoader: boolean = false;
   isEmpty: boolean = false;
 
-  constructor(private quizservice: QuizService, private router: Router) {}
+  constructor(
+    private quizservice: QuizService, 
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.showLoader = true;
@@ -38,6 +44,23 @@ export class ApplicantListComponent implements OnInit {
     this.quizservice.checkPlagiarism(this.data.proposal).subscribe((data) => {
       console.log(data);
       this.plagStat = data;
+    });
+  }
+
+  onViewClick(res){
+    let data = {
+      Recruit_obj: res.Recruit_obj,
+      Recruit_add_obj: res.Recruit_add_obj,
+      proposal: res.proposal,
+      similarity: res.similarity,
+      plagPercent: res.plagPercent,
+      At: res.At,
+      candi_skills: res.get_candidate_skills,
+      job_skills: res.get_job_skills
+    }
+    let dialogRef = this.dialog.open(JobViewDialogComponent, {
+      height: '400px',
+      data: data
     });
   }
 }
